3d4d9cdabc5dc2cf4cf68e221afcfafe5a44ca47
nroshdy92 <156032373+nroshdy92@users.noreply.github.com>
2024-01-10 18:50:56 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,49 @@
-variable "simple_nested_list" {​
 
-  type    = list(list(string))​
+variable "subscription_id"{
+  type=string
+}
 
-  default = [["a", "b"], ["c", "d"], ["e", "f"]]​
-
-}​
-
-locals{​
-
-simple_local_flattened_list = flatten(var.simple_nested_list)}​
-
-output "simple_flattened_list_result" {​
-
-  value = local.simple_local_flattened_list​
-
-}​
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "countNumber"{
+  type=number
+  default=0
+}
+variable "account_tier"{
+  type=string
+  default="Standard"
+}
+variable "myname"{
+  type=string
+}
+variable "account_replication_type"{
+  type=string
+  default="GRS"
+}
+variable "access_tier"{
+  type=string
+  default="Cool"
+}
+variable "cross_tenant_replication_enabled"{
+ type=bool
+ default=false
+}
+variable "prefix"{
+ type=string
+ default="mcit"
+}
+variable "component" {
+  type    = list
+  default = ["bastion", "frontproxy", "db", "infra"]
+}
+variable "environment"{
+ type=string
+ default="staging"
+}
