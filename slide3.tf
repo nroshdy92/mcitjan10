02191d9aac74c56975472051309a4bd632cb45a9
nroshdy92 <156032373+nroshdy92@users.noreply.github.com>
2024-01-10 19:57:09 -0500

    Create slide3.tf

---
+++ slide3.tf
@@ -0,0 +1,14 @@
+variable "nested_map" {
+type = map(map(string))
+default = {
+group1 = {"name" = "Alice", "age" = "25"}
+group2 = {"name" = "bob", "age" ="30"}
+}
+}​
+locals{
+flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])​
+}   ​
+
+output "flattened_map_result" {​
+  value = local.flattened_map​
+}
