0b4164ae692e05ac28c127a05e345d3713f9a8e2
nroshdy92 <156032373+nroshdy92@users.noreply.github.com>
2024-01-10 20:04:54 -0500

    Update slide3.tf

--- slide3.tf
+++ slide3.tf
@@ -4,7 +4,7 @@ default = {
 group1 = {"name" = "Alice", "age" = "25"}
 group2 = {"name" = "bob", "age" = "30"}
 }
-}​
+}
 locals{
 flattened_map  = flatten([for k, v in var.nested_map1 : [for kk, vv in v : { group = k, key = kk, value = vv }]])​
 }   ​
